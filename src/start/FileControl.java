c957e6db82e9f0e7713d871682d789626f383c93
BakaX <bagadiyi@gmail.com>
2018-08-06 01:41:40 +1200

    Add the save class to a txt file

--- src/start/FileControl.java
+++ src/start/FileControl.java
@@ -11,7 +11,40 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.File;
 
 public class FileControl {
-
+	public static void savemap(Player p) {
+		try {
+			
+		   try(PrintWriter fileWriter = new PrintWriter(new File("playerdata/data.txt"))){
+			   fileWriter.println(p.getName());
+			   fileWriter.println(p.getAttack());
+			   fileWriter.println(p.getDefence());
+			   fileWriter.println(p.getLevel());
+			   fileWriter.println(p.getHealthmax());
+			   fileWriter.println(p.getHealth());
+			   fileWriter.println(p.getMoney());
+			   fileWriter.println(p.getRow());
+			   fileWriter.println(p.getColumn());
+			   fileWriter.println(p.isM1defeat());
+			   fileWriter.println(p.isM2defeat());
+			   fileWriter.println(p.isM3defeat());
+			   fileWriter.println(p.getExp());
+			   fileWriter.println(p.getWeapon());
+			   fileWriter.println(p.getShield());
+			   for (Ability a : p.ablist.getAbilitylist()) {
+				   fileWriter.println(a.getName());
+			   }
+			   fileWriter.println("--SEPERATOR--");
+			   for (Entity e : p.enlist.getentityList()) {
+			       fileWriter.println(e.getName());
+			       fileWriter.println(e.getNum());
+			   }
+			   fileWriter.close();
+		   }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
